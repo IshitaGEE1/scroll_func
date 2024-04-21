fe61a86fb279c648a40cdff9cf0e601304df5947
Ishita Goswami <86228837+IshitaGEE1@users.noreply.github.com>
2024-04-21 23:56:42 +0530

    Update contentfulConfiguration.js

--- plugins/contentfulConfiguration.js
+++ plugins/contentfulConfiguration.js
@@ -4,14 +4,14 @@ function createContentfulConfiguration() {
   console.log('ENVIRONMENT', environment);
 
   return {
-    //space: 'q17uls4wkkdz',
-    //accessToken: process.env.CTF_PREVIEW
-      //? 'hRK6RDr_ZULih7PuJgVkSTSl9kDqYr7cqfh5KvdCvow'
-      //: '-hGtrR28CV2kk9-OQPRsx6rYDXNaI3WxAgshVJLBT6c',
     space: 'q17uls4wkkdz',
-    accessToken: 'hRK6RDr_ZULih7PuJgVkSTSl9kDqYr7cqfh5KvdCvow',
-    host: 'preview.contentful.com',
-    environment: 'master'
+    accessToken: process.env.CTF_PREVIEW
+    ? 'hRK6RDr_ZULih7PuJgVkSTSl9kDqYr7cqfh5KvdCvow'
+    : '-hGtrR28CV2kk9-OQPRsx6rYDXNaI3WxAgshVJLBT6c',
+    //space: 'q17uls4wkkdz',
+    //accessToken: 'hRK6RDr_ZULih7PuJgVkSTSl9kDqYr7cqfh5KvdCvow',
+    //host: 'preview.contentful.com',
+    //environment: 'master'
   };
 }
 
